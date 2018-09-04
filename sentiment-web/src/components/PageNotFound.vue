6615120b4b19004cd6d88174d29004477c71c7f5
LBEVAN <LukeRJBevan@gmail.com>
2018-09-04 17:56:02 +0100

    Handle Invalid request
    * Handle an invalid request in the webapp by redirecting to 404 Page Not Found.

---
+++ sentiment-web/src/components/PageNotFound.vue
@@ -0,0 +1,23 @@
+<template id="button-toggle-group">
+  <div class="wrapper">
+    <h1 class="display-2">Page Not Found</h1>
+    <h3 class="display-5">The page you requested was not found</h3>
+
+    <div class="section">
+      <form @submit.prevent="submit">
+        <button type="submit" class="btn btn-primary">Return to Homepage</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'PageNotFound',
+    methods: {
+      submit: function () {
+        this.$router.push({ name: 'CreateAnalysisRequest'})
+      }
+    }
+  }
+</script>
--- sentiment-web/src/router/index.js
+++ sentiment-web/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import CreateAnalysisRequest from '@/components/CreateAnalysisRequest'
+import ViewAnalysisRequest from '@/components/ViewAnalysisRequest'
+import PageNotFound from '@/components/PageNotFound'
 
 Vue.use(Router)
 
@@ -10,6 +12,19 @@ export default new Router({
       path: '/',
       name: 'CreateAnalysisRequest',
       component: CreateAnalysisRequest
+    },
+    {
+      path: '/request/:id',
+      name: 'ViewAnalysisRequest',
+      component: ViewAnalysisRequest
+    },
+    {
+      path: '/404',
+      component: PageNotFound
+    },
+    {
+      path: '*',
+      redirect: '/404'
     }
   ]
 })
